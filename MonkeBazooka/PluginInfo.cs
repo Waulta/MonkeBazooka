d56b16cb7c6d7d8c92d5d782102515e3a93f25b9
Waulta <81683867+Waulta@users.noreply.github.com>
2023-04-18 18:08:42 +0930

    bumped version number

--- MonkeBazooka/PluginInfo.cs
+++ MonkeBazooka/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.waulta.gorillatag.monkebazooka";
         public const string Name = "MonkeBazooka";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.2";
     }
 }
